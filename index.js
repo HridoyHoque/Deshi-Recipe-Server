e54e31747683b718d10581a1b3035d3a596f33b3
HridoyHoque <hhridoy155@gmail.com>
2023-05-04 15:48:00 +0600

    added 18 recipe item in json file

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+
+const chefs = require('./Data/Chefs.json')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Recipe is Running');
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedItems = chefs.find(r => r.id == id)
+    res.send(selectedItems)
 })
 
 app.listen(port,  () => {
